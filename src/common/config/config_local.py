55f354d0637b6d0f35214ddd9d1d7af01f2d55d4
Jaydeep <jaydeep@LAP-FVFWKF8DHV22.local>
2019-03-06 15:06:43 +0530

    changes

--- src/common/config/config_local.py
+++ src/common/config/config_local.py
@@ -5,6 +5,6 @@ DB_HOST = "127.0.0.1"
 DB_PORT = "3306"
 DB_NAME = "dataplatform_utils"
 
-WHITELISTED_SCHEMA_NAMES = ['match_maker_new']
+WHITELISTED_SCHEMA_NAMES = ['match_maker_new', 'managed_services_new', 'payment_new']
 
 
